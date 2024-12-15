79c54e232f0ad26f9554a461f53be95369ed3d69
modyelgen <modyelgen243@gmail.com>
2024-12-15 07:21:22 +0200

    add info in home again ag

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        cardColor: Colors.black
+
       ),
       home: const MyHomePage(title: 'Flutter Home Page'),
     );
